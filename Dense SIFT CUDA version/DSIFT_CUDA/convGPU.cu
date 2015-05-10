9109c688f0c03968aaa5f5389acc2bf9ce1ce69d
Eniac <xiecw.mail@gmail.com>
2015-05-10 15:29:08 +0800

    add the original DSIFT CPU version

---
+++ Dense SIFT CUDA version/DSIFT_CUDA/convGPU.cu
@@ -0,0 +1,224 @@
+#include <cuda.h> 
+#include <cuda_runtime.h>
+#include <assert.h>
+#include <windows.h>
+#include <iostream>
+#include "device_launch_parameters.h"
+#include "device_functions.h"
+#include "C:\ProgramData\NVIDIA Corporation\CUDA Samples\v6.5\common\inc\helper_cuda.h"
+#include "convGPU.h"
+
+
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+// Convolution kernel storage
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Row convolution filter
+////////////////////////////////////////////////////////////////////////////////
+#define   ROWS_BLOCKDIM_X 16
+#define   ROWS_BLOCKDIM_Y 4
+#define   ROWS_RESULT_STEPS 32 
+#define   ROWS_HALO_STEPS 1
+
+__global__ void convolutionRowsKernel(
+	float *d_Dst,
+	float *d_Src,
+	float *kernel,
+	int imageW,
+	int imageH,
+	int pitch
+	)
+{
+	__shared__ float s_Data[ROWS_BLOCKDIM_Y][(ROWS_RESULT_STEPS + 2 * ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X];
+
+	//Offset to the left halo edge
+	const int baseX = (blockIdx.x * ROWS_RESULT_STEPS - ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X + threadIdx.x;
+	const int baseY = blockIdx.y * ROWS_BLOCKDIM_Y + threadIdx.y;
+
+	d_Src += baseY * pitch + baseX;
+	d_Dst += baseY * pitch + baseX;
+
+	//Load main data
+#pragma unroll
+
+	for (int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++)
+	{
+		s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X] = d_Src[i * ROWS_BLOCKDIM_X];
+	}
+
+	//Load left halo
+#pragma unroll
+
+	for (int i = 0; i < ROWS_HALO_STEPS; i++)
+	{
+		s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X] = (baseX >= -i * ROWS_BLOCKDIM_X) ? d_Src[i * ROWS_BLOCKDIM_X] : 0;
+	}
+
+	//Load right halo
+#pragma unroll
+
+	for (int i = ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS + ROWS_HALO_STEPS; i++)
+	{
+		s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X] = (imageW - baseX > i * ROWS_BLOCKDIM_X) ? d_Src[i * ROWS_BLOCKDIM_X] : 0;
+	}
+
+	//Compute and store results
+	__syncthreads();
+#pragma unroll
+
+	for (int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++)
+	{
+		float sum = 0;
+
+#pragma unroll
+
+		for (int j = -KERNEL_RADIUS + 2; j <= KERNEL_RADIUS; j++)
+		{
+			sum += kernel[KERNEL_RADIUS - j] * s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X + j];
+		}
+
+		d_Dst[i * ROWS_BLOCKDIM_X] = sum;
+	}
+}
+
+void convolutionRowsGPU(
+	float *d_Dst,
+	float *d_Src,
+	float *kernel,
+	int imageW,
+	int imageH
+	)
+{
+	assert(ROWS_BLOCKDIM_X * ROWS_HALO_STEPS >= KERNEL_RADIUS);
+	//assert(imageW % (ROWS_RESULT_STEPS * ROWS_BLOCKDIM_X) == 0);
+	//assert(imageH % ROWS_BLOCKDIM_Y == 0);
+
+	dim3 blocks(imageW / (ROWS_RESULT_STEPS * ROWS_BLOCKDIM_X), imageH / ROWS_BLOCKDIM_Y);
+	dim3 threads(ROWS_BLOCKDIM_X, ROWS_BLOCKDIM_Y);
+	convolutionRowsKernel << <blocks, threads >> >(
+		d_Dst,
+		d_Src,
+		kernel,
+		imageW,
+		imageH,
+		imageW
+		);
+	getLastCudaError("convolutionRowsKernel() execution failed\n");
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Column convolution filter
+////////////////////////////////////////////////////////////////////////////////
+#define   COLUMNS_BLOCKDIM_X 16
+#define   COLUMNS_BLOCKDIM_Y 8
+#define COLUMNS_RESULT_STEPS 8
+#define   COLUMNS_HALO_STEPS 1
+
+__global__ void convolutionColumnsKernel(
+	float *d_Dst,
+	float *d_Src,
+	float *kernel,
+	int imageW,
+	int imageH,
+	int pitch
+	)
+{
+	__shared__ float s_Data[COLUMNS_BLOCKDIM_X][(COLUMNS_RESULT_STEPS + 2 * COLUMNS_HALO_STEPS) * COLUMNS_BLOCKDIM_Y + 1];
+
+	//Offset to the upper halo edge
+	const int baseX = blockIdx.x * COLUMNS_BLOCKDIM_X + threadIdx.x;
+	const int baseY = (blockIdx.y * COLUMNS_RESULT_STEPS - COLUMNS_HALO_STEPS) * COLUMNS_BLOCKDIM_Y + threadIdx.y;
+	d_Src += baseY * pitch + baseX;
+	d_Dst += baseY * pitch + baseX;
+
+	//Main data
+#pragma unroll
+
+	for (int i = COLUMNS_HALO_STEPS; i < COLUMNS_HALO_STEPS + COLUMNS_RESULT_STEPS; i++)
+	{
+		s_Data[threadIdx.x][threadIdx.y + i * COLUMNS_BLOCKDIM_Y] = d_Src[i * COLUMNS_BLOCKDIM_Y * pitch];
+	}
+
+	//Upper halo
+#pragma unroll
+
+	for (int i = 0; i < COLUMNS_HALO_STEPS; i++)
+	{
+		s_Data[threadIdx.x][threadIdx.y + i * COLUMNS_BLOCKDIM_Y] = (baseY >= -i * COLUMNS_BLOCKDIM_Y) ? d_Src[i * COLUMNS_BLOCKDIM_Y * pitch] : 0;
+	}
+
+	//Lower halo
+#pragma unroll
+
+	for (int i = COLUMNS_HALO_STEPS + COLUMNS_RESULT_STEPS; i < COLUMNS_HALO_STEPS + COLUMNS_RESULT_STEPS + COLUMNS_HALO_STEPS; i++)
+	{
+		s_Data[threadIdx.x][threadIdx.y + i * COLUMNS_BLOCKDIM_Y] = (imageH - baseY > i * COLUMNS_BLOCKDIM_Y) ? d_Src[i * COLUMNS_BLOCKDIM_Y * pitch] : 0;
+	}
+
+	//Compute and store results
+	__syncthreads();
+#pragma unroll
+
+	for (int i = COLUMNS_HALO_STEPS; i < COLUMNS_HALO_STEPS + COLUMNS_RESULT_STEPS; i++)
+	{
+		float sum = 0;
+#pragma unroll
+
+		for (int j = -KERNEL_RADIUS + 2; j <= KERNEL_RADIUS; j++)
+		{
+			sum += kernel[KERNEL_RADIUS - j] * s_Data[threadIdx.x][threadIdx.y + i * COLUMNS_BLOCKDIM_Y + j];
+		}
+
+		d_Dst[i * COLUMNS_BLOCKDIM_Y * pitch] = sum;
+	}
+}
+
+void convolutionColumnsGPU(
+	float *d_Dst,
+	float *d_Src,
+	float *kernel,
+	int imageW,
+	int imageH
+	)
+{
+	assert(COLUMNS_BLOCKDIM_Y * COLUMNS_HALO_STEPS >= KERNEL_RADIUS);
+	//assert(imageW % COLUMNS_BLOCKDIM_X == 0);
+	//assert(imageH % (COLUMNS_RESULT_STEPS * COLUMNS_BLOCKDIM_Y) == 0);
+
+	dim3 blocks(imageW / COLUMNS_BLOCKDIM_X, imageH / (COLUMNS_RESULT_STEPS * COLUMNS_BLOCKDIM_Y));
+	dim3 threads(COLUMNS_BLOCKDIM_X, COLUMNS_BLOCKDIM_Y);
+
+	convolutionColumnsKernel << <blocks, threads >> >(
+		d_Dst,
+		d_Src,
+		kernel,
+		imageW,
+		imageH,
+		imageW
+		);
+	getLastCudaError("convolutionColumnsKernel() execution failed\n");
+}
+
+__global__ void getDescrKernel(float *resDescr, float *d_Output, int baseX, int baseY, int imWidth)
+{
+	*(resDescr + threadIdx.x + blockIdx.x * 32 + (threadIdx.y + blockIdx.y * 32) * imWidth) = d_Output[(threadIdx.x + blockIdx.x * 32 + baseX) * 1 +
+		(threadIdx.y + blockIdx.y * 32 + baseY) * imWidth];
+}
+
+void getDescr(float *resDescr, float *d_Output, int outHeight, int outWidth, int baseX, int baseY, int imWidth)
+{
+
+	dim3 blocks(outWidth / 32, outHeight / 32);
+	dim3 threads(32, 32);
+	getDescrKernel << <blocks, threads >> >(resDescr, d_Output, baseX, baseY, imWidth);
+	getLastCudaError("convolutionRowsKernel() execution failed\n");
+	
+}
+
---
+++ Dense SIFT CUDA version/DSIFT_CUDA/fastMath.h
@@ -0,0 +1,72 @@
+#ifndef FASTMATH_H
+#define FASTMATH_H
+#include <cmath>
+
+#define DSIFT_EPSILON_F 1.19209290E-07F
+#define DSIFT_PI 3.141592653589793
+
+inline float fast_atan2_f(float y, float x)
+{
+	float angle, r;
+	float const c3 = 0.1821F;
+	float const c1 = 0.9675F;
+	float abs_y = fabs(y) + DSIFT_EPSILON_F;
+
+	if (x >= 0) 
+	{
+		r = (x - abs_y) / (x + abs_y);
+		angle = (float)(DSIFT_PI / 4);
+	}
+	else 
+	{
+		r = (x + abs_y) / (abs_y - x);
+		angle = (float)(3 * DSIFT_PI / 4);
+	}
+	angle += (c3*r*r - c1) * r;
+	return (y < 0) ? -angle : angle;
+};
+
+inline float fast_resqrt_f(float x)
+{
+	/* 32-bit version */
+	union 
+	{
+		float x;
+		int  i;
+	} u;
+
+	float xhalf = (float) 0.5 * x;
+
+	/* convert floating point value in RAW integer */
+	u.x = x;
+
+	/* gives initial guess y0 */
+	u.i = 0x5f3759df - (u.i >> 1);
+	/*u.i = 0xdf59375f - (u.i>>1);*/
+
+	/* two Newton steps */
+	u.x = u.x * ((float) 1.5 - xhalf*u.x*u.x);
+	u.x = u.x * ((float) 1.5 - xhalf*u.x*u.x);
+	return u.x;
+};
+
+inline float fast_sqrt_f(float x)
+{
+	return (x < 1e-8) ? 0 : x * fast_resqrt_f(x);
+};
+
+inline float mod_2pi_f(float x)
+{
+	while (x >(float)(2 * DSIFT_PI)) x -= (float)(2 * DSIFT_PI);
+	while (x < 0.0F) x += (float)(2 * DSIFT_PI);
+	return x;
+};
+
+inline long int floor_f(float x)
+{
+	long int xi = (long int)x;
+	if (x >= 0 || (float)xi == x) return xi;
+	else return xi - 1;
+};
+
+#endif
